5147a2959bb9114fc567cf0520c0195dde4feada
flazo0 <flazo0@proton.me>
2026-07-10 14:52:40 -0300

    Change port from 3000 to 3012

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3012;
 
 app.use(cors());
 app.use(express.json());
